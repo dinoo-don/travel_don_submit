6421c26c1092234250cd0fba8326a361f8042651
dinoo-don <don_nguyen93@hotmail.com>
2022-11-14 15:00:01 +1100

    travel_don_submit

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ function checkButton()
                      } 
                   }     
             function validateForm()
-                {
-                let textField = document.getElementById("textField").value;
+                 {
+                  let textField = document.getElementById("textField").value;
                     if ( textField == "")
                         {
                         window.alert( "Please enter in missing field");
                         return false;
                         }
 
-                let mymail=document.myform.myemail.value;
-                let attherate=mymail.indexOf("@");
+                  let mymail=document.myform.myemail.value;
+                  let attherate=mymail.indexOf("@");
                         let atthedot=mymail.lastIndexOf(".");
              
                     if (attherate<1 || atthedot<attherate+2 || atthedot>=mymail.length)
@@ -29,22 +29,22 @@ function checkButton()
                                   return false;
                         }
                
-                let curdate= new Date();
-                let departure= Date(document.myform.mydate.value);
-                  if (departure<curdate)
+                  let curdate= new Date();
+                  let departure= Date(document.myform.mydate.value);
+                        if (departure<curdate)
                         {
                             alert( "enter departure date");
                             return false;
                         }
 
-               let mobile1=(document.myform.mobile.value)
-                  if (mobile1.length==10)
+                  let mobile1=(document.myform.mobile.value)
+                        if (mobile1.length==10)
                         {
                         return true
                         }
-                  else      
+                        else      
                         {
                            alert( "enter the correct 10 digits number");
                            return false;
                         }              
-               }
+                 }
